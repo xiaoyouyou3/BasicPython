5551b4bac14d6cb2feb505d3d21e2b9bc9e0c4df
xiaoyouyou3 <xiaolinyousi3@gmail.com>
2024-01-03 02:04:59 +0900

    制御構文の解答2

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -5,7 +5,7 @@ from math import sin, pi
 # -----------
 #積分範囲
 a = 0
-b = 0.5**pi
+b = 0.5*pi
 #分割数
 n = 100
 #各区間の幅
@@ -17,10 +17,3 @@ for i in range(1, n+1):
     answer += (h/2)*(sin(a+(i-1)*h) + sin(a+i*h))
 
 print(answer)
-
-ans = 0.5 * (sin(a) + sin(b))
-for j in range(1,n):
-    x = a + j * h
-    ans += sin(x)
-ans *= h
-print(ans)
